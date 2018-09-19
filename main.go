09568e254551660d9cc6b0a2bb421aedbd35a7cc
Sergey Novikov <snov@snov.me>
2018-09-19 17:33:26 +0200

    wip

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"compress/gzip"
+	"encoding/csv"
+	"errors"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -14,25 +16,46 @@ import (
 )
 
 type LogEntry struct {
-	Type      string `json:"type"`
-	Timestamp string `json:"@timestamp"`
-	Elb       string `json:"type"`
+	Type                   string `json:"type"`
+	Timestamp              string `json:"@timestamp"`
+	Elb                    string `json:"type"`
+	Client                 string `json:"client"`
+	Target                 string `json:"target"`
+	RequestProcessingTime  string `json:"request_processing_time"`
+	TargetProcessingTime   string `json:"target_processing_time"`
+	ResponseProcessingTime string `json:"response_processing_time"`
+	ElbStatusCode          string `json:"elb_status_code"`
+	TargetStatusCode       string `json:"target_status_code"`
+	ReceivedBytes          string `json:"received_bytes"`
+	SentBytes              string `json:"sent_bytes"`
+	Request                string `json:"request"`
+	UserAgent              string `json:"user_agent"`
+	SslCipher              string `json:"ssl_cipher"`
+	SslProtocol            string `json:"ssl_protocol"`
+	TargetGroupArn         string `json:"target_group_arn"`
+	TraceId                string `json:"trace_id"`
+	DomainName             string `json:"domain_name"`
+	ChosenCertArn          string `json:"chosen_cert_arn"`
+	MatchedRulePriority    string `json:"matched_rule_priority"`
+	RequestCreationTime    string `json:"request_creation_time"`
+	ActionsExecuted        string `json:"actions_executed"`
+	RedirectUrl            string `json:"redirect_url"`
 }
 
-func handler(s3Event events.S3Event) (string, error) {
-
-	for i, rec := range s3Event.Records {
-		fmt.Print(i)
-		bucket := rec.S3.Bucket.Name
-		key := rec.S3.Object.Key
-		log.Printf("processing %v/%v", bucket, key)
-		readS3File(bucket, key)
+func handler(s3Event events.S3Event) error {
+	for _, rec := range s3Event.Records {
+		closer, _ := readS3File(rec.S3.Bucket.Name, rec.S3.Object.Key)
+		logEntries := read(closer)
+		for _, l := range logEntries {
+			fmt.Println(*l)
+		}
 	}
 
-	return "boom", nil
+	return nil
 }
 
-func readS3File(bucket string, key string) {
+func readS3File(bucket string, key string) (io.ReadCloser, error) {
+	log.Printf("reading %v/%v", bucket, key)
 	cfg, _ := external.LoadDefaultAWSConfig()
 	s3svc := s3.New(cfg)
 	req := s3svc.GetObjectRequest(&s3.GetObjectInput{
@@ -41,27 +64,129 @@ func readS3File(bucket string, key string) {
 	})
 	resp, err := req.Send()
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		gzReader, _ := gzip.NewReader(resp.Body)
-		br := bufio.NewReader(gzReader)
-		for {
-			line, err := br.ReadString('\n')
-			if err == io.EOF {
-				break
-			} else if err != nil {
-				// boom
-			}
-			line = strings.Trim(line, "\n")
-			cols := strings.Split(line, " ")
-			logEntry := &LogEntry{
-				Type: cols[0],
-				Timestamp: cols[1],
-				Elb: cols[2],
-			}
-			fmt.Println(logEntry)
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
+
+func read(closer io.ReadCloser) []*LogEntry {
+	defer func() {
+		closer.Close()
+		if r := recover(); r != nil {
+			log.Printf("recovered from '%v', skipping entry", r)
+		}
+	}()
+
+	var logEntries []*LogEntry
+	gzReader, _ := gzip.NewReader(closer)
+	br := bufio.NewReader(gzReader)
+	for {
+		line, err := br.ReadString('\n')
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Println(err)
+			break
+		}
+		logEntry, err := createLogEntryFromLine(line)
+		if err == nil {
+			logEntries = append(logEntries, logEntry)
+		} else {
+			log.Println(err)
 		}
 	}
+	return logEntries
+}
+
+func readLogEntries(line string) (entry *LogEntry, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New(fmt.Sprintf("Unable to parse line '%v', error: %v", line, r))
+		}
+	}()
+
+	line = strings.Trim(line, "\n")
+	r := csv.NewReader(strings.NewReader(line))
+	r.Comma = ' '
+
+	records, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	cols := records[0]
+	return &LogEntry{
+		Type:                   cols[0],
+		Timestamp:              cols[1],
+		Elb:                    cols[2],
+		Client:                 cols[3],
+		Target:                 cols[4],
+		RequestProcessingTime:  cols[5],
+		TargetProcessingTime:   cols[6],
+		ResponseProcessingTime: cols[7],
+		ElbStatusCode:          cols[8],
+		TargetStatusCode:       cols[9],
+		ReceivedBytes:          cols[10],
+		SentBytes:              cols[11],
+		Request:                strings.Trim(cols[12], "\""),
+		UserAgent:              strings.Trim(cols[13], "\""),
+		SslCipher:              cols[14],
+		SslProtocol:            cols[15],
+		TargetGroupArn:         cols[16],
+		TraceId:                strings.Trim(cols[17], "\""),
+		DomainName:             strings.Trim(cols[18], "\""),
+		ChosenCertArn:          strings.Trim(cols[19], "\""),
+		MatchedRulePriority:    cols[20],
+		RequestCreationTime:    cols[21],
+		ActionsExecuted:        strings.Trim(cols[22], "\""),
+		RedirectUrl:            strings.Trim(cols[23], "\""),
+	}, nil
+}
+
+func createLogEntryFromLine(line string) (entry *LogEntry, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New(fmt.Sprintf("Unable to parse line '%v', error: %v", line, r))
+		}
+	}()
+
+	line = strings.Trim(line, "\n")
+	r := csv.NewReader(strings.NewReader(line))
+	r.Comma = ' '
+
+	records, err := r.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	cols := records[0]
+	return &LogEntry{
+		Type:                   cols[0],
+		Timestamp:              cols[1],
+		Elb:                    cols[2],
+		Client:                 cols[3],
+		Target:                 cols[4],
+		RequestProcessingTime:  cols[5],
+		TargetProcessingTime:   cols[6],
+		ResponseProcessingTime: cols[7],
+		ElbStatusCode:          cols[8],
+		TargetStatusCode:       cols[9],
+		ReceivedBytes:          cols[10],
+		SentBytes:              cols[11],
+		Request:                strings.Trim(cols[12], "\""),
+		UserAgent:              strings.Trim(cols[13], "\""),
+		SslCipher:              cols[14],
+		SslProtocol:            cols[15],
+		TargetGroupArn:         cols[16],
+		TraceId:                strings.Trim(cols[17], "\""),
+		DomainName:             strings.Trim(cols[18], "\""),
+		ChosenCertArn:          strings.Trim(cols[19], "\""),
+		MatchedRulePriority:    cols[20],
+		RequestCreationTime:    cols[21],
+		ActionsExecuted:        strings.Trim(cols[22], "\""),
+		RedirectUrl:            strings.Trim(cols[23], "\""),
+	}, nil
 }
 
 //func processLogFile(bucket string, key string) {
--- main_test.go
+++ main_test.go
@@ -17,11 +17,34 @@ func TestHandler(t *testing.T) {
 			t.Errorf("could not unmarshal event: %v", err)
 		}
 
-		_, err := handler(s3Event)
+		err := handler(s3Event)
 		if err != nil {
 			t.Errorf("unexpected error: %v", err)
 		}
 	})
+}
+
+func TestCreateLogEntryFromLine(t *testing.T) {
+	logEntry, err := createLogEntryFromLine(`http 2018-09-18T21:38:37.519183Z app/test1/7f050ffab5373730 95.90.211.80:4254 172.31.7.183:80 0.001 0.000 0.000 200 200 461 654 "GET http://test1-332132803.eu-west-1.elb.amazonaws.com:80/ HTTP/1.1" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36" - - arn:aws:elasticloadbalancing:eu-west-1:328305143014:targetgroup/target-group-1/76784dcf3348e51f "Root=1-5ba1705d-5617cf32286ecfb8172f73cc" "-" "-" 0 2018-09-18T21:38:37.518000Z "forward" "-" `)
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if logEntry.Type != "http" {
+		t.Errorf("invalid type")
+	}
+	if logEntry.Timestamp != "2018-09-18T21:38:37.519183Z" {
+		t.Errorf("invalid timestamp")
+	}
+	if logEntry.Client != "95.90.211.80:4254" {
+		t.Errorf("invalid client")
+	}
+	if logEntry.Target != "172.31.7.183:80" {
+		t.Errorf("invalid target")
+	}
+	if logEntry.Request != "GET http://test1-332132803.eu-west-1.elb.amazonaws.com:80/ HTTP/1.1" {
+		t.Errorf("invalid request, %v", logEntry.Request)
+	}
 }
 
 func readJsonFromFile(t *testing.T, inputFile string) []byte {
